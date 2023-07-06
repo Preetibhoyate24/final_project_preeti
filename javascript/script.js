5e7e9ab5e7c0530941df1c913ec0550ce535e27f
Preetibhoyate24 <prtkalyani094@gmail.com>
2023-07-07 01:19:09 +0530

    second

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+// add to cart
+
+const btncart = document.querySelector('btn-cart');
+const cart = document.querySelector('.cart');
+const btnclose = document.querySelector('#cart-close');
+
+btncart.addEventListener(.click'()=>(
+    cart.classList.add('cart-active');
+));
