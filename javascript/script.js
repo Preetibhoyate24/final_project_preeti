a676acd3ff07a1402d58a4518b84a0935f3114fd
Preetibhoyate24 <prtkalyani094@gmail.com>
2023-07-08 01:25:50 +0530

    third

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-// add to cart
-
-const btncart = document.querySelector('btn-cart');
-const cart = document.querySelector('.cart');
-const btnclose = document.querySelector('#cart-close');
-
-btncart.addEventListener(.click'()=>(
-    cart.classList.add('cart-active');
-));
